4ef427dc09f284e2bf9664b75f1e633def9feb57
softhea <liviusandulache@yahoo.com>
2021-01-13 09:32:30 +0200

    changed the comments to give more details

--- migrations.php
+++ migrations.php
@@ -1,72 +1,69 @@
 <?php
 
-ini_set('display_errors', 1);
-
 /**
- * create database if needed
+ * creates database `invatam_sa_programam` if it doesn't exist
  */
 $databaseConnection = mysqli_connect('localhost', 'root', '');
 if ($databaseConnection !== false) {
+    /**
+     * searches for 'invatam_sa_programam' database
+     */
     $query = "SHOW DATABASES WHERE `DATABASE` = 'invatam_sa_programam'";
     $result = mysqli_query($databaseConnection, $query);
     $database = mysqli_fetch_assoc($result);
     if ($database === null) {
         $query = "CREATE DATABASE invatam_sa_programam DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci";
-        $result = mysqli_query($databaseConnection, $query);
+        mysqli_query($databaseConnection, $query);
         $affectedRows = mysqli_affected_rows($databaseConnection);
         if ($affectedRows < 1) {
-            die('database could not be created!');
+            die('Database `invatam_sa_programam` could not be created!');
         }
     }
 } else {
-    die('MySQL Server Details are incorrect!');
+    die('Could not connect to MySQL Server!');
 }
 
 /**
- * use the database
+ * closes the current connection. creates a new one that uses the database too
+ * this connection will be used in index.php too
  */
 mysqli_close($databaseConnection);
-$databaseConnection = mysqli_connect(
-    'localhost',
-    'root',
-    '',
-    'invatam_sa_programam'
-);
+$databaseConnection = mysqli_connect('localhost', 'root', '', 'invatam_sa_programam');
 
 /**
- * create table users if needed
+ * creates table `users` if it doesn't exist
  */
 $query =
     "CREATE TABLE IF NOT EXISTS `users` (
-    `id` int(10) UNSIGNED NOT NULL,
-    `username` varchar(50) NOT NULL,
-    `password` varchar(20) NOT NULL
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+        `id` int(10) UNSIGNED NOT NULL,
+        `username` varchar(50) NOT NULL,
+        `password` varchar(20) NOT NULL
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8";
 $result = mysqli_query($databaseConnection, $query);
 if ($result === false) {
-    die('Error creating table users!');
+    die('Could not create table `users`!');
 }
 
 /**
- * adds indexes for id and username
+ * adds indexes for `id` and `username` columns: PRIMARY KEY for `id` and KEY for `username`
  */
 $query =
     "ALTER TABLE `users`
     ADD PRIMARY KEY (`id`),
-    ADD KEY `username` (`username`);";
+    ADD KEY `username` (`username`)";
 $result = mysqli_query($databaseConnection, $query);
 if ($result !== false) {
     /**
-     * adds auto increment for id
+     * adds AUTO_INCREMENT for `id` column
      */
     $query =
         "ALTER TABLE `users`
         MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;";
-    $result = mysqli_query($databaseConnection, $query);
+    mysqli_query($databaseConnection, $query);
 }
 
 /**
- * inserts admin users if needed
+ * inserts two admin users if user with 'admin' username doesn't exist
  */
 $query = "SELECT id FROM users WHERE username = 'admin'";
 $result = mysqli_query($databaseConnection, $query);
