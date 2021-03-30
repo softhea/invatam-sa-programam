ed8ac614a4857c191306b2803fda119fd0d32925
Liviu Sandulache <liviusandulache@yahoo.com>
2021-03-30 22:42:55 +0300

    added episode 03

---
+++ 03/index.php
@@ -0,0 +1,33 @@
+<?php
+
+session_start();
+
+if (
+        isset($_POST['login']) &&
+        $_POST['username'] === 'admin' &&
+        $_POST['password'] === 'parola'
+) {
+    $_SESSION['logged'] = true;
+}
+
+$logged = false;	
+if (isset($_SESSION['logged'])) {
+	$logged = true;	
+}
+
+include 'menu.php';
+?>
+
+<?php if (!$logged): ?>
+    <form method="POST">
+        <input type="text" name="username" value="" placeholder="Username">
+        <input type="password" name="password" value="" placeholder="Password">
+        <input type="submit" name="login" value="Login">
+    </form>
+<?php endif; ?>
+
+<h1>Invatam Sa Programam</h1>
+
+<h3>PHP & MySQL</h3>
+
+<p>HTML</p>
