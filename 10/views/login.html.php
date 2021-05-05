9a8f0b3ee5fe6e4dcb8c79e79275a3b3a61001b5
softhea <liviusandulache@yahoo.com>
2021-05-05 22:47:16 +0300

    added spaces

--- 10/views/login.html.php
+++ 10/views/login.html.php
@@ -2,9 +2,9 @@
 	<input type="text" name="username" value="" placeholder="Username">
 	<input type="password" name="password" value="" placeholder="Password">
 	<input type="submit" name="login" value="Login">
-<a href="register.php">Register</a>
+	<a href="register.php">Register</a>
 </form>
 
 <?php if ($error !== ''): ?>
-<p><?=$error?></p>
+	<p><?=$error?></p>
 <?php endif; ?>
