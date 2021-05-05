b993ddd4087540c6133437953528857bf24a1c60
softhea <liviusandulache@yahoo.com>
2021-05-05 22:48:45 +0300

    spaces

--- 10/views/users.html.php
+++ 10/views/users.html.php
@@ -5,22 +5,22 @@
 <?php if (count($users) > 0): ?>
     <table border="1">
         <tr>
-            <th>User ID</th>
-            <th>Username</th>
-			<th>Email</th>
-			<th>Active</th>
-			<th>Actions</th>
+            	<th>User ID</th>
+            	<th>Username</th>
+		<th>Email</th>
+		<th>Active</th>
+		<th>Actions</th>
         </tr>
  
-	    <?php foreach ($users as $user): ?>
-            <tr>
-                <td><?=$user['id']?></td>
-                <td><?=$user['username']?></td>
-				<td><?=$user['email']?></td>
-				<td><?=$user['register_code'] === null ? 'Yes' : 'No'?></td>
-				<td><a href="delete.php?user_id=<?=$user['id']?>" >Delete</a></td>
-            </tr>
-	    <?php endforeach; ?>
+	<?php foreach ($users as $user): ?>
+            	<tr>
+                	<td><?=$user['id']?></td>
+                	<td><?=$user['username']?></td>
+			<td><?=$user['email']?></td>
+			<td><?=$user['register_code'] === null ? 'Yes' : 'No'?></td>
+			<td><a href="delete.php?user_id=<?=$user['id']?>" >Delete</a></td>
+            	</tr>
+	<?php endforeach; ?>
 	
     </table>
 <?php endif; ?>
