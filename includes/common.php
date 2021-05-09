78b514cf8b23d2f7217eb8823eae335c4710279e
liviu.sandulache <liviu.sandulache@rei-d-services.com>
2021-05-09 10:54:06 +0300

    created gitignore and config.php

--- includes/common.php
+++ includes/common.php
@@ -2,11 +2,7 @@
 
 session_start();
 
-const SITE_URL = 'http://localhost/invatam-sa-programam';
-const DB_HOST = 'localhost';
-const DB_USER = 'root';
-const DB_PASSWORD = '';
-const DB_NAME = 'invatam_sa_programam';
+require 'config.php';
 
 function redirect($uri)
 {
