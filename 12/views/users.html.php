f89b9ea444ce1cb7ed4f81fbbb908a1c8636abde
Liviu Sandulache <liviusandulache@yahoo.com>
2021-05-10 09:47:40 +0300

    spaces

--- 12/views/users.html.php
+++ 12/views/users.html.php
@@ -3,24 +3,23 @@
 <p><a href="add-user.php">Add New User</a></p>
 
 <?php if (count($users) > 0): ?>
-    <table border="1">
-        <tr>
+	<table border="1">
+		<tr>
 			<th>User ID</th>
 			<th>Username</th>
 			<th>Email</th>
 			<th>Active</th>
 			<th>Actions</th>
-        </tr>
- 
-	<?php foreach ($users as $user): ?>
-            <tr>
-               	<td><?=$user['id']?></td>
-               	<td><?=$user['username']?></td>
+		</tr>
+
+		<?php foreach ($users as $user): ?>
+			<tr>
+				<td><?=$user['id']?></td>
+				<td><?=$user['username']?></td>
 				<td><?=$user['email']?></td>
 				<td><?=$user['register_code'] === null ? 'Yes' : 'No'?></td>
 				<td><a href="delete.php?user_id=<?=$user['id']?>" >Delete</a></td>
-            </tr>
-	<?php endforeach; ?>
-	
-    </table>
+			</tr>
+		<?php endforeach; ?>
+	</table>
 <?php endif; ?>
