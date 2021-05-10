077629e68143edaf5162b1b765a2231d3329a2c9
Liviu Sandulache <liviusandulache@yahoo.com>
2021-05-10 09:17:25 +0300

    spaces

--- 06/index.php
+++ 06/index.php
@@ -6,7 +6,7 @@ $error = '';
 if (isset($_POST['login'])) {
 	$query = "SELECT id FROM users WHERE username = '".$_POST['username']."' AND password = '".md5($_POST['password'])."' AND register_code IS NULL";
 
-    $databaseConnection = mysqli_connect('localhost', 'root', '', 'invatam_sa_programam');
+	$databaseConnection = mysqli_connect('localhost', 'root', '', 'invatam_sa_programam');
 	$result = mysqli_query($databaseConnection, $query);
 	$user = mysqli_fetch_assoc($result);
 	if ($user !== null) {
